e056e186bf60e359fdf287d2f81cee295c22167a
Adriano Elias Daniel <adrianoedaniel@gmail.com>
2025-05-15 14:57:50 -0300

    Aula Deploy

---
+++ src/main/java/com/deploy/AulaDeployApplication.java
@@ -0,0 +1,13 @@
+package com.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaDeployApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/deploy/repository/TurmaRepository.java
@@ -0,0 +1,14 @@
+package com.deploy.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.deploy.entities.Turma;
+
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+	public List<Turma> findByNome(String nome);
+
+}
+
+//List<Turma> findByNomeAndRenda(String nome, String descricao);
